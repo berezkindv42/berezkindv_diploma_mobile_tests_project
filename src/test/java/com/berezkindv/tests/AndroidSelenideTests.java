882c447f7b66338742368210b793647bfff848df
DaymianDark <nightkonn@gmail.com>
2022-04-23 15:55:44 +0300

    test test added

--- src/test/java/com/berezkindv/tests/AndroidSelenideTests.java
+++ src/test/java/com/berezkindv/tests/AndroidSelenideTests.java
@@ -8,23 +8,30 @@ import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AndroidSelenideTests extends TestBase {
 
     @Test
     @Tag("mobile")
-    void searchTest() {
-        step("Skip onboarding page", () -> back());
-
-        step("Type search", () -> {
-            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("wikipedia");
-        });
-        step("Verify content found", () ->
-                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
+    void testTest() {
+        assertTrue(true);
     }
 
+//    @Test
+//    @Tag("mobile")
+//    void searchTest() {
+//        step("Skip onboarding page", () -> back());
+//
+//        step("Type search", () -> {
+//            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+//            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("wikipedia");
+//        });
+//        step("Verify content found", () ->
+//                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+//                        .shouldHave(sizeGreaterThan(0)));
+//    }
+//
 //    @Test
 //    @Tag("mobile")
 //    void onBoardingPagesTest() {
